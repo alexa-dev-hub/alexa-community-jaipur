71f4dd788841b5f59cf0741e220784479d610aeb
Abir <abir.pal899@gmail.com>
2020-09-09 23:11:38 +0530

    Added useEffect hook for GitHub Contributors

--- client/src/pages/blog/Sidebar.js
+++ client/src/pages/blog/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
@@ -21,6 +21,12 @@ const useStyles = makeStyles((theme) => ({
 export default function Sidebar(props) {
   const classes = useStyles();
   const { webinar, archives, social } = props;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://api.github.com/repos/alexa-dev-hub/alexa-community-jaipur/contributors")
+      .then(response => response.json())
+      .then(data => setData(data));
+  });
 
   return (
     <Grid item xs={12} md={4}>
@@ -59,6 +65,14 @@ export default function Sidebar(props) {
           </ScrollLink>
         </Typography>
       ))}
+<Typography variant="h6" gutterBottom className={classes.sidebarSection}>
+        Contributors
+      </Typography>
+{
+data.map(contributor => (
+          <li key={contributor.id}> <img src={contributor.avatar_url}> <a href={contributor.url}>{contributor.login}</a></li>
+        ))
+}
       <Typography variant="h6" gutterBottom className={classes.sidebarSection}>
         Connect
       </Typography>
