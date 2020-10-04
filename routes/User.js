078c7bd7d8b248dddd6093a3e9fe6f6a15c5c7be
Abir <abir.pal899@gmail.com>
2020-10-04 16:07:11 +0530

    Removed Backend Code

--- routes/User.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const userRouter = express.Router();
-
-userRouter.get("/", (req, res) => {
-  res.status(200).json({ message: { msgBody: "Hit User", msgError: false } });
-});
-
-module.exports = userRouter;
