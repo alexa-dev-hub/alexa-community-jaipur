a7cb1d5a01a0c6bc470b4615f26281431f4e5b33
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-10-12 14:40:28 +0200

    Fix Header on Mobile View #51
    Changed media queries 500px to 600px and 400px to 450px

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -15,12 +15,14 @@ import Menu from "@material-ui/core/Menu";
 
 const useStyles = makeStyles((theme) => ({
   toolbar: {
+	display: "flex",
+	justifyContent: "space-between",
     borderBottom: `1px solid ${theme.palette.divider}`,
     backgroundColor: "whiteSmoke",
     color: "black",
   },
   toolbarTitle: {
-    flex: 1,
+    flex: "inherit",
     fontFamily: "Segoe UI",
   },
   image: {
@@ -101,12 +103,12 @@ export default function Header(props) {
 			  </div>
             </div>
           </Typography>
-
-          <Button size="small" onClick={handleMenu}>
-            Blog
-            <ArrowDropDownIcon />
-          </Button>
-          <Menu
+          <div>
+           <Button size="small" onClick={handleMenu}>
+             Blog
+             <ArrowDropDownIcon />
+           </Button>
+           <Menu
             id="menu-appbar"
             anchorEl={anchorEl}
             anchorOrigin={{
@@ -120,17 +122,18 @@ export default function Header(props) {
             }}
             open={open}
             onClose={handleClose}
-          >
-            <MenuItem onClick={handleClose}>Blog 1</MenuItem>
-            <MenuItem onClick={handleClose}>Blog 2</MenuItem>
-          </Menu>
+           >
+             <MenuItem onClick={handleClose}>Blog 1</MenuItem>
+             <MenuItem onClick={handleClose}>Blog 2</MenuItem>
+           </Menu>
 
-          <Button variant="contained" size="small" className={classes.button}>
-            Login
-          </Button>
-          {/* <Button variant='contained' size='small' className={classes.button}>
-            Logout
-          </Button> */}
+           <Button variant="contained" size="small" className={classes.button}>
+             Login
+           </Button>
+           {/* <Button variant='contained' size='small' className={classes.button}>
+             Logout
+           </Button> */}
+		 </div>
         </Toolbar>
       </AppBar>
       <div style={{ marginTop: "70px" }}>
